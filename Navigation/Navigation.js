51517f897f303bd9703f2d6cd19996eeff394ec2
Michiel Bervoets <michiel.bervoets@student.kdg.be>
2018-03-14 09:39:19 +0100

    Update

--- Navigation/Navigation.js
+++
@@ -1,9 +0,0 @@
-import {
-    StackNavigator,
-} from 'react-navigation';
-
-import Chatdetails from '../Components/Chat/ChatScreenDetails';
-
-const Navigation = StackNavigator({
-    ChatDetails: { screen: Chatdetails }
-});
